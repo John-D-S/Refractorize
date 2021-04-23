989bfe627d6053c6dd33d0701fdc308e7b53e6aa
John-D-S <68535837+John-D-S@users.noreply.github.com>
2021-04-23 12:08:11 +1000

    improved playercontroller script

--- Assets/Refractorize/Scripts/CharacterController.cs
+++ Assets/Refractorize/Scripts/CharacterController.cs
@@ -11,6 +11,11 @@ public class CharacterController : MonoBehaviour
     private float movementSpeed = 0.5f;
     [SerializeField]
     private float rotationSpeed = 1f;
+    [SerializeField]
+    private float maxObjectHeldDistance = 1;
+
+    private GameObject heldObject;
+    private bool rotatingObject;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
@@ -25,12 +30,34 @@ public class CharacterController : MonoBehaviour
     {
         Vector2 rawMovementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
         movementDirection = Vector2.Lerp(movementDirection, rawMovementDirection, 1);
+        
+        Debug.DrawRay(transform.position, gameObject.transform.up);
+        
+        if (Input.GetButtonDown("LeftClick") && !heldObject)
+        {
+            
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, gameObject.transform.up, maxObjectHeldDistance);
+            
+
+            // If it hits something...
+            if (hit.collider.gameObject.GetComponent<Pickupable>())
+            {
+                Debug.Log("hello");
+                heldObject = hit.collider.gameObject;
+                heldObject.transform.parent = gameObject.transform;
+                heldObject.transform.position = Vector2.up;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        //rb.AddTorque(Vector2.Dot(transform.lo transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * rotationSpeed);
-        rb.AddTorque(Vector2.Dot(- transform.right, (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized) * rotationSpeed);
+        PhysRotateTowardMouse();
+    }
+
+    void PhysRotateTowardMouse()
+    {
+        rb.AddTorque(Vector2.Dot(-transform.right, (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized) * rotationSpeed);
         rb.AddForce(movementDirection);
     }
 }
